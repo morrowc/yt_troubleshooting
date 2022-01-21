e377a06eec99bc596ea0129d330afe400cd1c425
Chris Morrow <morrowc@ops-netman.net>
2022-01-21 15:10:18 +0000

    Starting a transitition to python3.
    Migrate most py2 things to py3, start fixing the data collection
    and debug the YT redirector query.

--- yt_troubleshooting.py
+++ yt_troubleshooting.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 #
 # Do some testing and data collection for YT problems.
 # Follow standard troubleshooting guidelines for YT issues.
@@ -9,7 +9,7 @@ import os
 import re
 import smtplib
 import socket
-import StringIO
+from io import StringIO
 import subprocess
 import sys
 
@@ -31,7 +31,7 @@ def EmailResult(rcptto, content, mailfrom, mailhost):
     mailhost: a string, the destination to send mail through.
   """
   # Create a 'file' object type to Mime-ify.
-  string = StringIO.StringIO()
+  string = StringIO()
   string.write('\n'.join(content))
   string.seek(0)
 
@@ -50,20 +50,20 @@ def EmailResult(rcptto, content, mailfrom, mailhost):
     s.sendmail(mailfrom, rcptto, msg.as_string())
     s.quit()
   except smtplib.SMTPConnectError as e:
-    print 'Failed to send mail, connect error: %s' % e
+    print( 'Failed to send mail, connect error: %s' % e)
     sys.exit(1)
   except smtplib.SMTPDataError as e:
-    print 'Failed to send mail, data error: %s' % e
+    print( 'Failed to send mail, data error: %s' % e)
     sys.exit(1)
   except smtplib.SMTPException as e:
-    print 'Failed to send mail, Exception: %s' % e
+    print( 'Failed to send mail, Exception: %s' % e)
     sys.exit(1)
   except smtplib.SMTPHeloError as e:
-    print 'Failed to send mail, Error in Helo: %s' % e
+    print( 'Failed to send mail, Error in Helo: %s' % e)
     sys.exit(1)
   except socket.error as e:
-    print 'Failed to connect to the smtp/mailhost: %s' % mailhost
-    print msg.as_string()
+    print( 'Failed to connect to the smtp/mailhost: %s' % mailhost)
+    print( msg.as_string())
     sys.exit(1)
   
 
@@ -153,25 +153,25 @@ def V4Mappings():
   v4_redir = []
   # Find the redirector location(s).
   v4_redir = Resolver('redirector.c.youtube.com')
-  print 'v4 redirector addresses: %s' % ', '.join(v4_redir)
+  print( 'v4 redirector addresses: %s' % ', '.join(v4_redir))
   result.append('v4 redirector.c.youtube.com: %s' % ', '.join(v4_redir))
-  print 'Tracerouting to all v4 redirector locations in turn.'
+  print( 'Tracerouting to all v4 redirector locations in turn.')
   result.append('Traceroute results to v4_redirector destinations.')
   for host in v4_redir:
     result.append(Traceroute(host))
 
   # Find the mapping location returned from the redirector.
-  print 'Asking for the v4 map location.'
+  print( 'Asking for the v4 map location.')
   v4_mapping = Wget('http://redirector.c.youtube.com/report_mapping', '4')
   result.append('v4 mapping:\n%s' % v4_mapping)
-  m = MAPPING_RE.match(v4_mapping)
+  m = MAPPING_RE.match(v4_mapping.decode())
   if m:
     map_addr = m.group(1)
   else:
-    print 'No mapping address found for ipv4, stopping processing here.'
+    print( 'No mapping address found for ipv4, stopping processing here.')
     return result
 
-  print 'v4 Mapping address: %s' % map_addr
+  print( 'v4 Mapping address: %s' % map_addr)
   if '-' in map_addr:
     result.append('Traceroute to GGC node:\n')
     result.append(Traceroute('%s.ba.l.google.com' % map_addr))
@@ -189,25 +189,25 @@ def V6Mappings():
   v6_redir = []
   # Find the redirector over v6.
   v6_redir = Resolver('redirector.c.youtube.com', socket.AF_INET6)
-  print 'v6 redirector sites: %s' % ', '.join(v6_redir)
+  print( 'v6 redirector sites: %s' % ', '.join(v6_redir))
   result.append('v6 redirector.c.youtube.com: %s' % ', '.join(v6_redir))
-  print 'Tracerouting to all v6 redir locations in turn.'
+  print( 'Tracerouting to all v6 redir locations in turn.')
   result.append('Traceroute results to v6_redirector destinations.')
   for host in v6_redir:
     result.append(Traceroute(host, '6'))
 
   # Find the mapping location over v6, returned from the redirector..
-  print 'Asking for v6 map location.'
+  print( 'Asking for v6 map location.')
   v6_mapping = Wget('http://redirector.c.youtube.com/report_mapping', '6')
   result.append('v6 mapping:\n%s' % v6_mapping)
-  m = MAPPING_RE.match(v6_mapping)
+  m = MAPPING_RE.match(v6_mapping.decode())
   if m:
     map_addr = m.group(1)
   else:
-    print 'No mapping address found for ipv6, stopping processing here.'
+    print( 'No mapping address found for ipv6, stopping processing here.')
     return result
 
-  print 'v6 Mapping address: %s' % map_addr
+  print( 'v6 Mapping address: %s' % map_addr)
   if '-' in map_addr:
     result.append('Traceroutes to v6 GGC node.\n')
     result.append(Traceroute(map_addr, '6'))
@@ -236,39 +236,41 @@ def main():
 
   # v6 if available.
   if socket.has_ipv6:
-    print 'Tested for v6 connectivity, doing v6 tests.'
+    print( 'Tested for v6 connectivity, doing v6 tests.')
     result.extend(V6Mappings())
   else:
     result.append('NO IPv6 AVAILABLE, all v6 tests skipped.')
   
   # Get the hostname/mapping for video playback.
-  print 'Looking up v4 stream location.'
+  print( 'Looking up v4 stream location.')
   loc_v4 = Wget('"http://redirector.c.youtube.com/videoplayback?id=1&itag=2"',
                  '4', '/dev/null', '', '2>&1 | grep Location | head -1')
-  m = re.search(r'^Location:\s+http://(.+\.youtube\.com)/vid.*$', loc_v4)
+  m = re.search(r'^Location:\s+http://(.+\.youtube\.com)/vid.*$', loc_v4.decode())
   if m:
     loc_v4 = m.group(1)
 
-  print 'v4 stream location: %s' % loc_v4.strip()
+  print( 'v4 stream location: %s' % loc_v4.strip())
   result.append('IPv4 streaming host: %s' % loc_v4)
 
   # Traceroute and save that result as well.
-  print 'Traceroute to the v4 streamer.'
+  print( 'Traceroute to the v4 streamer.')
+  print('Streamer: %s' % loc_v4)
   result.append(Traceroute(loc_v4))
 
   if socket.has_ipv6:
     loc_v6 = Wget('"http://redirector.c.youtube.com/videoplayback?id=1&itag=2"',
                    '6', '/dev/null', '', '2>&1 | grep Location | head -1')
-    m = re.search(r'^Location:\s+http://(.+\.youtube\.com)/vid.*$', loc_v6)
+    m = re.search(r'^Location:\s+http://(.+\.youtube\.com)/vid.*$', loc_v6.decode())
     if m:
       loc_v6 = m.group(1)
     result.append('IPv6 streaming host: %s' % loc_v6)
-    print 'v6 streamer location: %s' % loc_v6
-    print 'Tracerouting to the v6 streaming location.'
+    print( 'v6 streamer location: %s' % loc_v6)
+    print( 'Tracerouting to the v6 streaming location.')
     result.append(Traceroute(loc_v6, '6'))
 
-  print 'Emailing results now.'
+  print( 'Emailing results now.')
   EmailResult(options.email, result, options.mailfrom, options.mailhost)
+
 
 
 if __name__ == '__main__':
